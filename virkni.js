2118e75cee1f23a61f91c7fd112fba5c8aa724aa
alli959 <alg35@hi.is>
2018-02-11 19:14:24 +0000

    pepp

--- virkni.js
+++ virkni.js
@@ -1,3 +1,87 @@
+
+
+
+function game(array){
+    document.close();
+    var message = document.createElement("h2");
+    var input = document.createElement("input");
+    var table = document.createElement("table");
+    table = makeTableHTML(board);
+    document.write(table);
+    document.body.appendChild(input);
+    input.onchange = function(){
+        if(input.value == 1){
+            if(turn){
+                var k = playerMove(board, 'P1');
+                if(typeof(k) != "string"){
+                    turn = !turn
+                }
+                game(k);
+            }
+            else{
+                var k = computerMove(board, 'C1');
+                if(typeof(k) != "string"){
+                    turn = !turn
+                }
+                game(k);
+            }
+        }
+        if(input.value == 2){
+            if(turn){
+                var k = playerMove(board, 'P2');
+                if(typeof(k) != "string"){
+                    turn = !turn
+                }
+                game(k);
+            }
+            else{
+                var k = computerMove(board, 'C2');
+                if(typeof(k) != "string"){
+                    turn = !turn
+                }
+                game(k);
+            }
+        }
+        if(input.value == 3){
+            if(turn){
+                var k = playerMove(board, 'P3');
+                if(typeof(k) != "string"){
+                    turn = !turn
+                }
+                game(k);
+            }
+            else{
+                var k = computerMove(board, 'C3');
+                if(typeof(k) != "string"){
+                    turn = !turn
+                }
+                game(k);
+            }
+        }
+    }
+}
+    
+var turn = true;
+
+
+
+
+
+
+function makeTableHTML(myArray) {
+    var result = "<table border=1>";
+    for(var i=0; i<myArray.length; i++) {
+        result += "<tr>";
+        for(var j=0; j<myArray[i].length; j++){
+            result += "<td>"+myArray[i][j]+"</td>";
+        }
+        result += "</tr>";
+    }
+    result += "</table>";
+    
+    return result;
+}
+
 var board = [
     [null,'C1','C2','C3', null],
     ['P1',null, null, null, null],
@@ -11,9 +95,8 @@ var board = [
 
 function indexFinder(arr, value){
     for(var i = 0; i<arr.length-1; i++){
-        console.log(arr.length);
         var x = board[i].indexOf(value)
-        console.log(x);
+        if(x === 4) break;
         if(x != -1){
             return[i,x];
         }
@@ -27,10 +110,7 @@ function playerMove(arr, value){
     if(typeof(valuePosition) === "string"){
         return valuePosition;
     }
-    if(valuePosition[0] === 3){
-        arr[valuePosition[0]][valuePosition[1]+1] = value;
-        arr[valuePosition[0]][valuePosition[1]] = null;
-    }
+
     if(arr[valuePosition[0]][valuePosition[1]+1] != null){
         if(arr[valuePosition[0]][valuePosition[1]+2] != null){
             return "wrong move!";
@@ -48,11 +128,32 @@ function playerMove(arr, value){
     return arr;
 }
 
-console.log(playerMove(board,'P1'))
-console.log(playerMove(board,'P1'))
-console.log(playerMove(board,'P1'))
-console.log(playerMove(board,'P1'))
-console.log(playerMove(board,'P1'))
+
+
+function computerMove(arr, value){
+    var valuePosition = indexFinder(arr,value);
+    if(typeof(valuePosition) === "string"){
+        return valuePosition;
+    }
+    if(arr[valuePosition[0]+1][valuePosition[1]] != null){
+        if(arr[valuePosition[0]+2][valuePosition[1]] != null){
+            return "wrong move!";
+        }
+        else{
+            arr[valuePosition[0]+2][valuePosition[1]] = value;
+            arr[valuePosition[0]][valuePosition[1]] = null;
+        }
+    }
+
+    else{
+        arr[valuePosition[0]+1][valuePosition[1]] = value;
+        arr[valuePosition[0]][valuePosition[1]] = null;
+    }
+    return arr;
+}
 
 
 
+
+
+game(board);
